ffbe3698bb28bd93c1f4d695f5d46fbd99abf414
removal <56557003+killed@users.noreply.github.com>
2020-11-04 21:49:08 +0000

    Fixes
    Fixed follow and unfollow handler changes and forgot to update.

--- commands/Instagram/follow.js
+++ commands/Instagram/follow.js
@@ -14,7 +14,7 @@ exports.run = (bot, message, suffix, help) => {
     if (!suffix)
         return utility.parameters("follow", message);
 
-    request.get("www.instagram.com", `/web/search/topsearch/?context=blended&query=${suffix}&rank_token=0.8730298813193997&include_reel=false`, config.instagram.sessionId, config.instagram.userAgents.browser).then(body => {
+    request.get("www.instagram.com", `/web/search/topsearch/?context=blended&query=${suffix}&include_reel=false`, config.instagram.sessionId, config.instagram.userAgents.browser).then(body => {
         body = JSON.parse(body);
 
         if (body.users.length < 1)
@@ -25,7 +25,7 @@ exports.run = (bot, message, suffix, help) => {
         if (!result || result.length < 1)
             return utility.error(`No user with the username ${suffix} exists`, message);
         else
-            request.post(`/web/friendships/${result[0].user.pk}/follow/`, {}, config.instagram.sessionId).then(body => {
+            request.postData("www.instagram.com", `/web/friendships/${result[0].user.pk}/follow/`, "", config.instagram.sessionId, config.instagram.userAgents.browser).then(body => {
                 utility.success(`Successfully followed [@${suffix}](https://www.instagram.com/${suffix})`, message);
             }).catch(error => {
                 utility.error(`An error occurred while trying to follow ${suffix}`, message);
--- commands/Instagram/unfollow.js
+++ commands/Instagram/unfollow.js
@@ -14,7 +14,7 @@ exports.run = (bot, message, suffix, help) => {
     if (!suffix)
         return utility.parameters("unfollow", message);
 
-    request.get("www.instagram.com", `/web/search/topsearch/?context=blended&query=${suffix}&rank_token=0.8730298813193997&include_reel=false`, config.instagram.sessionId, config.instagram.userAgents.browser).then(body => {
+    request.get("www.instagram.com", `/web/search/topsearch/?context=blended&query=${suffix}&include_reel=false`, config.instagram.sessionId, config.instagram.userAgents.browser).then(body => {
         body = JSON.parse(body);
 
         if (body.users.length < 1)
@@ -25,7 +25,7 @@ exports.run = (bot, message, suffix, help) => {
         if (!result || result.length < 1)
             return utility.error(`No user with the username ${suffix} exists`, message);
         else
-            request.post(`/web/friendships/${result[0].user.pk}/unfollow/`, {}, config.instagram.sessionId).then(body => {
+            request.postData("www.instagram.com", `/web/friendships/${result[0].user.pk}/unfollow/`, "", config.instagram.sessionId, config.instagram.userAgents.browser).then(body => {
                 utility.success(`Successfully unfollowed [@${suffix}](https://www.instagram.com/${suffix})`, message);
             }).catch(error => {
                 utility.error(`An error occurred while trying to unfollow ${suffix}`, message);
